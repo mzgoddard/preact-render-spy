8752080d4059481c769790e5fc3c2fcd902fd1f7
Michael "Z" Goddard <mzgoddard@gmail.com>
2017-06-14 09:40:20 -0400

    Build on poc with .find, .output, .simulate

--- src/preact-render-spy.js
+++ src/preact-render-spy.js
@@ -1,30 +1,113 @@
 const {render} = require('preact');
 
-const spyComponent = Component => {
-  const scratch = document.createDocumentFragment();
-  const context = {
-    component: null,
-    Spy: null,
-    scratch,
-    output: null,
-    render(vdom) {
-      vdom.nodeName = context.Spy;
-      context.component = render(vdom, context.scratch);
-      return context;
-    },
-  };
+const events = require('./events');
+
+const spyWalk = (spy, vdom) => {
+  if (typeof vdom.nodeName === 'function' && !vdom.nodeName.isSpy) {
+    vdom = Object.assign({}, vdom, {nodeName: createSpy(spy, vdom.nodeName)});
+  }
+  else {
+    vdom = Object.assign({}, vdom, {
+      children: vdom.children.map(child => spyWalk(spy, child)),
+    });
+  }
+  return vdom;
+};
+
+const createSpy = (spy, Component) => {
   class Spy extends Component {
     render(...args) {
-      context.output = super.render(...args);
-      return context.output;
+      const output = super.render(...args);
+      spy._output(this, output);
+      return spyWalk(spy, output);
     }
   }
-  context.Spy = Spy;
-  return context;
+
+  Spy.isSpy = true;
+
+  return Spy;
+};
+
+class SpyWrapper {
+  constructor(Component) {
+    this.domMap = new Map();
+    this.fragment = document.createDocumentFragment();
+  }
+
+  _output(nodeName, vdom) {
+    if (typeof nodeName === 'string') {
+      this.domMap.set(nodeName, vdom);
+    }
+    else {
+      this.domMap.set(nodeName.constructor, vdom);
+    }
+  }
+
+  output(selector) {
+    return this._getVDom(this.fragment.querySelector(selector));
+  }
+
+  find(selector) {
+    return new FindWrapper(this, this.fragment, selector);
+  }
+
+  _getVDom(element) {
+    const indexPath = [];
+    while (element.parentNode !== this.fragment) {
+      const parentNode = element.parentNode;
+      indexPath.unshift(Array.from(parentNode.children).indexOf(element));
+      element = parentNode;
+    }
+    let vdom = this.domMap.get(this._rootConstructor);
+    while (indexPath.length) {
+      while (typeof vdom.nodeName === 'function') {
+        vdom = this.domMap.get(vdom.nodeName);
+      }
+      vdom = vdom.children[indexPath.shift()];
+    }
+    while (typeof vdom.nodeName === 'function') {
+      vdom = this.domMap.get(vdom.nodeName);
+    }
+    return vdom;
+  }
+
+  render(vdom) {
+    vdom = spyWalk(this, vdom);
+    this._rootConstructor = vdom.nodeName;
+    if (typeof vdom.nodeName === 'string') {
+      this._output(vdom.nodeName, vdom);
+    }
+    this.component = render(vdom, this.fragment);
+    return this;
+  }
+}
+
+class FindWrapper {
+  constructor(spy, root, selector) {
+    this.spy = spy;
+    this.root = root;
+    this.selector = selector;
+    Array.from(root.querySelectorAll(selector))
+    .forEach((element, index) => {
+      this[index] = element;
+      this.length = index + 1;
+    });
+  }
+
+  simulate(event, ...args) {
+    for (let i = 0; i < this.length; i++) {
+      const vdom = this.spy._getVDom(this[i]);
+      const handle = vdom.attributes && vdom.attributes[events[event]];
+      if (handle) {
+        handle(...args);
+      }
+    }
+    return new Promise(resolve => setTimeout(resolve, 0));
+  }
 }
 
 const renderSpy = vdom => {
-  return spyComponent(vdom.nodeName).render(vdom);
+  return new SpyWrapper().render(vdom);
 };
 
 module.exports = renderSpy;
--- src/preact-render-spy.test.js
+++ src/preact-render-spy.test.js
@@ -2,12 +2,74 @@ const {h, Component} = require('preact');
 
 const renderSpy = require('./preact-render-spy');
 
-it('renders into scratch', async () => {
+it('renders into fragment', async () => {
   class Node extends Component {
     render() {
       return <div />;
     }
   }
   const context = renderSpy(<Node />);
-  expect(context.output).toEqual(<div />);
+  expect(context.output('div')).toEqual(<div />);
+  expect(context.fragment.children.length).toBe(1);
+  expect(context.fragment.children[0].tagName).toBe('DIV');
+  expect(context.find('div').length).toBe(1);
+});
+
+it('renders props', async () => {
+  class Node extends Component {
+    render({className}) {
+      return <div class={className} />;
+    }
+  }
+  const context = renderSpy(<Node className="node" />);
+  expect(context.output('div')).toEqual(<div class="node" />);
+  expect(context.find('.node').length).toBe(1);
+});
+
+it('renders changes', async () => {
+  class Node extends Component {
+    render({className}) {
+      return <div class={className} />;
+    }
+  }
+  const context = renderSpy(<Node className="node" />);
+  context.render(<Node className="node2" />);
+  expect(context.output('div')).toEqual(<div class="node2" />);
+  expect(context.find('.node2').length).toBe(1);
+});
+
+it('renders change on click', async () => {
+  class Node extends Component {
+    constructor(...args) {
+      super(...args);
+
+      this.state = {count: 0};
+      this.onClick = this.onClick.bind(this);
+    }
+
+    onClick() {
+      this.setState({count: this.state.count + 1});
+    }
+
+    render({className}, {count}) {
+      return <div class={className + count} onClick={this.onClick} />;
+    }
+  }
+  const context = renderSpy(<Node className="node" />);
+  expect(context.output('div').attributes.class).toEqual('node0');
+  await context.find('div').simulate('click');
+  expect(context.output('div').attributes.class).toEqual('node1');
+});
+
+it('renders multiple components', () => {
+  class Node extends Component {
+    render({count}) {
+      return <div class={'node' + count} />;
+    }
+  }
+  const context = renderSpy(<div><Node count="1" /><Node count="2" /></div>);
+  expect(context.find('.node1').length).toBe(1);
+  expect(context.output('.node1')).toEqual(<div class="node1" />);
+  expect(context.find('.node2').length).toBe(1);
+  expect(context.output('.node2')).toEqual(<div class="node2" />);
 });
