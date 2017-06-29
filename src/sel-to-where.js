f5e33c9be5c0cd77d956303d944c56e07b2e9a29
Corey Frang <corey@bocoup.com>
2017-06-29 15:51:41 -0400

    fixup! Only allow JSX in .test.js

--- src/sel-to-where.js
+++ src/sel-to-where.js
@@ -8,9 +8,9 @@ const selToWhere = sel => {
   else if (/^\[/.test(sel)) {
     return {attributes: {[sel.substring(1, sel.length - 1)]: null}};
   }
-  
+
   return {nodeName: sel};
-  
+
 };
 
 module.exports = {
