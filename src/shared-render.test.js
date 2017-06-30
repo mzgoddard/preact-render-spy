27de1e96f05ffa5d0604396f66225444772431c1
Michael "Z" Goddard <mzgoddard@gmail.com>
2017-06-30 03:10:55 -0400

    Pull the shared tests component types out of tests for reuse

--- src/shared-render.test.js
+++ src/shared-render.test.js
@@ -3,13 +3,47 @@ const {h, Component} = require('preact');
 const {deep, shallow} = require('./preact-render-spy');
 
 const sharedTests = (name, func) => {
-  it(`${name}: renders into fragment`, async () => {
-    class Node extends Component {
-      render() {
-        return <div />;
-      }
+  class Div extends Component {
+    render() {
+      return <div />;
     }
-    const context = func(<Node />);
+  }
+
+  class DivChildren extends Component {
+    render({children}) {
+      return <div>{children}</div>;
+    }
+  }
+
+  class ClickCount extends Component {
+    constructor(...args) {
+      super(...args);
+
+      this.state = {count: 0};
+      this.onClick = this.onClick.bind(this);
+    }
+
+    onClick() {
+      this.setState({count: this.state.count + 1});
+    }
+
+    render({}, {count}) {
+      return <div onClick={this.onClick}>{count}</div>;
+    }
+  }
+
+  class DivCount extends Component {
+    render({count}) {
+      return <div>{count}</div>;
+    }
+  }
+
+  const DivCountStateless = ({count}) => <div>{count}</div>;
+
+  const NullStateless = () => null;
+
+  it(`${name}: renders into fragment`, async () => {
+    const context = func(<Div />);
     expect(context.fragment.children.length).toBe(1);
     expect(context.fragment.children[0].tagName).toBe('DIV');
     expect(context.find('div').length).toBe(1);
@@ -17,57 +51,26 @@ const sharedTests = (name, func) => {
   });
 
   it(`${name}: renders props`, async () => {
-    class Node extends Component {
-      render({children}) {
-        return <div>{children}</div>;
-      }
-    }
-    const context = func(<Node>node</Node>);
+    const context = func(<DivChildren>node</DivChildren>);
     expect(context.find('div').length).toBe(1);
     expect(context.find('div').text('node')).toBeTruthy();
   });
 
   it(`${name}: renders changes`, async () => {
-    class Node extends Component {
-      render({children}) {
-        return <div>{children}</div>;
-      }
-    }
-    const context = func(<Node>node</Node>);
-    context.render(<Node>node2</Node>);
+    const context = func(<DivChildren>node</DivChildren>);
+    context.render(<DivChildren>node2</DivChildren>);
     expect(context.find('div').contains('node2')).toBeTruthy();
   });
 
   it(`${name}: renders change on click`, async () => {
-    class Node extends Component {
-      constructor(...args) {
-        super(...args);
-
-        this.state = {count: 0};
-        this.onClick = this.onClick.bind(this);
-      }
-
-      onClick() {
-        this.setState({count: this.state.count + 1});
-      }
-
-      render({}, {count}) {
-        return <div onClick={this.onClick}>{count}</div>;
-      }
-    }
-    const context = func(<Node/>);
+    const context = func(<ClickCount />);
     expect(context.find('div').contains('0')).toBeTruthy();
     context.find('div').simulate('click');
     expect(context.find('div').contains('1')).toBeTruthy();
   });
 
   it(`${name}: renders multiple components`, () => {
-    class Node extends Component {
-      render({count}) {
-        return <div>{count}</div>;
-      }
-    }
-    const context = func(<div><Node count="1" /><Node count="2" /></div>);
+    const context = func(<div><DivCount count="1" /><DivCount count="2" /></div>);
     expect(context.find('div').contains('1')).toBeTruthy();
     expect(context.find('div').contains(<div>1</div>)).toBeTruthy();
     expect(context.find('div').contains('2')).toBeTruthy();
@@ -75,15 +78,13 @@ const sharedTests = (name, func) => {
   });
 
   it(`${name}: renders stateless components`, () => {
-    const Node = ({count}) => <div>{count}</div>;
-    const context = func(<Node count="1" />);
+    const context = func(<DivCountStateless count="1" />);
     expect(context.find('div').length).toBe(1);
     expect(context.find('div').contains('1')).toBeTruthy();
   });
 
   it(`${name}: renders components with null children`, () => {
-    const Null = () => null;
-    const context = func(<div><Null />text</div>);
+    const context = func(<div><NullStateless />text</div>);
     expect(context.find('div').text()).toBe('text');
   });
 };
