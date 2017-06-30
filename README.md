6bc686bfd20b57c2b3feb2ca30cd8853648c45e1
Mx Corey Frang <gnarf37@gmail.com>
2017-06-30 14:32:20 -0400

    Add README (#18)
    * Add README
    
    * Rename SpyWrapper RenderContext
    
    * Add an example
    
    * Add some futher examples
    
    * Add support to readme

--- README.md
+++ README.md
@@ -1,3 +1,149 @@
 # preact-render-spy
 
 [![Build Status](https://travis-ci.org/mzgoddard/preact-render-spy.svg?branch=master)](https://travis-ci.org/mzgoddard/preact-render-spy)
+
+
+preact-render-spy is a package designed to cover many of the use cases for testing
+preact components.  The API is roughly modeled after enzyme, but we do not support
+as many options currently.
+
+## Support:
+
+We do our best to support Node.JS v6.11.0 and up, and speficially testing with jest, though other test runners should have no problems.
+
+## Expressive Testing Example:
+
+```jsx
+import {h} from 'preact';
+import {shallow} from 'preact-render-spy';
+import Testable from './testable';
+
+it('lets you do cool things with preact components', () => {
+  const context = shallow(<Testable />);
+  expect(context.find('div').contains(<a>link</a>)).toBeTruthy();
+  context.find('[onClick]').simulate('click');
+  expect(context.find('a').text()).toBe('clicked');
+});
+```
+
+## How it works
+
+The main render method takes some arbitrary JSX and replaces any component nodes with
+spies which wrap the component.  These spies then look at the rendered output of the
+second level of JSX and stores a big map of all the JSX virtual DOM nodes that have
+been created by your component.
+
+There is also concept of limiting the `depth` of the rendering such that it will only resolve
+a certain number of levels deep in the component tree.  The `depth` of the default renderer
+is set to `Infinity`, and we provide another renderer called `shallow` to render with
+`{ depth: 1 }`.
+
+## Exported Methods
+
+### `deep(jsx, {depth = Infinity} = {})`
+Creates a new `RenderContext` and renders using `opts.depth` to specify how many components deep
+it should allow the renderer to render.  Also exported as `render` and `default`.
+
+Example:
+```jsx
+const Deeper = () => <div>Until Infinity!</div>;
+const Second = () => <Deeper />;
+const First = () => <Second />;
+
+let context;
+context = deep(<First />);
+expect(context.find('div').text()).toBe('Until Infinity!');
+
+context = deep(<First />, { depth: 2 });
+// We rendered First and Second, but not Deeper, so we never render a <div>
+expect(context.find('div').length).toBe(0);
+```
+
+### `shallow(jsx)`
+Creates a new `RenderContext` with `{ depth: 1 }`.
+
+### `RenderContext#find(selector)`
+Given a rendered context, `find` accepts a "css like" language of selectors to search through the
+rendered vdom for given nodes.  **NOTE:** We only support this very limited set of "selectors", and no nesting.
+We may expand this selector language in future versions, but it acheives our goals so far!
+
+* `find('.selector')` - searches for any nodes with `class` or `className` attribute that matches `selector`
+* `find('#selector')` - searches for any nodes with an `id` attribute that matches `selector`
+* `find('[selector]')` - searches for any nodes which have an attribute named `selector`
+* `find('Selector')` - searches for any nodes which have a nodeName that matches `Selector`,
+  this wills search for function/classes whos `name` is `Selector`, or `displayName` is `Selector`.
+  If the `Selector` starts with a lower case letter, it will also check for tags like `div`.
+
+This will return you a `FindWrapper` which has other useful methods for testing.
+
+### `RenderContext#render(jsx)`
+Re-renders the root level jsx node using the same depth initially requested.  **NOTE:** When preact re-renders this way,
+it will not reuse components, so if you want to test `componentWillReceiveProps` you will need to use a test wrapper component.
+
+Example:
+
+```jsx
+const Node = ({name}) => <div>{name}</div>
+const context = shallow(<Node name="example" />);
+expect(context.find('div').text()).toBe('example');
+
+context.render(<Node name="second" />);
+expect(context.find('div').text()).toBe('second');
+```
+
+### `FindWrapper`
+Contains a selection of nodes from `RenderContext#find(selector)`.
+Has numeric indexed properties and length like an array.
+
+### `FindWrapper#at(index)`
+Returns another `FindWrapper` at the specific index in the selection.  Similar to `wrapper[0]` but will
+allow using other `FindWrapper` methods on the result.
+
+### `FindWrapper#attr(name)`
+Requires a single node selection to work.
+Returns the value of the `name` attribute on the jsx node.
+
+### `FindWrapper#attrs()`
+Requires a single node selection to work.
+Returns a copy of the attributes passed to the jsx node.
+
+### `FindWrapper#contains(vdom)`
+Searches for any children matching the vdom or text passed.
+
+### `FindWrapper#text()`
+Returns the flattened string of any text children of any child component.
+
+### `FindWrapper#simulate(event, ...args)`
+Looks for an attribute properly named `onEvent` or `onEventCapture` and calls it, passing the arguments.
+
+### `FindWrapper#output()`
+Requires a single Component or functional node.  Returns the raw vdom output of the given component.
+
+## Examples
+
+There are many examples in the source files.  Some [tests specific to shallow](https://github.com/mzgoddard/preact-render-spy/blob/master/src/shallow-render.test.js), [tests specific to deep](https://github.com/mzgoddard/preact-render-spy/blob/master/src/deep-render.test.js), and many more [tests against both](https://github.com/mzgoddard/preact-render-spy/blob/master/src/shared-render.test.js).
+
+### Simulate Clicks:
+```jsx
+class Node extends Component {
+  constructor(...args) {
+    super(...args);
+
+    this.state = {count: 0};
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    this.setState({count: this.state.count + 1});
+  }
+
+  render({}, {count}) {
+    return <div onClick={this.onClick}>{count}</div>;
+  }
+}
+const context = shallow(<Node/>);
+expect(context.find('div').contains('0')).toBeTruthy();
+context.find('[onClick]').simulate('click');
+expect(context.find('div').contains('1')).toBeTruthy();
+```
+
--- src/preact-render-spy.js
+++ src/preact-render-spy.js
@@ -8,14 +8,14 @@ const config = {
   SPY_PRIVATE_KEY: 'SPY_PRIVATE_KEY',
 };
 
-const spyWalk = (spy, vdom, depth) => {
+const spyWalk = (context, vdom, depth) => {
   if (!vdom) {
     return vdom;
   }
   if (typeof vdom.nodeName === 'function' && !vdom.nodeName.isSpy) {
     vdom = Object.assign({}, vdom, {
-      nodeName: (depth > spy.renderedDepth ? createNoopSpy : createSpy)(
-        spy, vdom.nodeName,
+      nodeName: (depth > context.renderedDepth ? createNoopSpy : createSpy)(
+        context, vdom.nodeName,
       ),
       attributes: Object.assign({}, vdom.attributes, {
         [config.SPY_PRIVATE_KEY]: {vdom, depth},
@@ -24,7 +24,7 @@ const spyWalk = (spy, vdom, depth) => {
   }
   else if (vdom.children) {
     vdom = Object.assign({}, vdom, {
-      children: vdom.children.map(child => spyWalk(spy, child, depth)),
+      children: vdom.children.map(child => spyWalk(context, child, depth)),
     });
   }
   return vdom;
@@ -36,14 +36,14 @@ const popSpyKey = _props => {
   return [spyKey, spyDepth, _props];
 };
 
-const setVDom = (spy, spyKey, vdom) => {
-  spy.vdomMap.set(spyKey, vdom);
+const setVDom = (context, spyKey, vdom) => {
+  context.vdomMap.set(spyKey, vdom);
   return vdom;
 };
 
 const NoopSpy = ()=>{};
 
-const _createNoopSpy = (spy, Component) => {
+const _createNoopSpy = (context, Component) => {
   return function(_props) {
     const [,, props] = popSpyKey(_props);
     const vdom = {
@@ -58,71 +58,71 @@ const _createNoopSpy = (spy, Component) => {
   };
 };
 
-const createFuncSpy = (spy, Component) => {
+const createFuncSpy = (context, Component) => {
   return function(_props, ...args) {
     const [spyKey, depth, props] = popSpyKey(_props);
     const output = Component.call(this, props, ...args);
-    return spyWalk(spy, setVDom(spy, spyKey, output), depth + 1);
+    return spyWalk(context, setVDom(context, spyKey, output), depth + 1);
   };
 };
 
-const createClassSpy = (spy, Component) => {
+const createClassSpy = (context, Component) => {
   class Spy extends Component {
     constructor(_props, ...args) {
       const [spyKey, spyDepth, props] = popSpyKey(_props);
       super(props, ...args);
-      spy.keyMap.set(this, spyKey);
-      spy.depthMap.set(this, spyDepth);
+      context.keyMap.set(this, spyKey);
+      context.depthMap.set(this, spyDepth);
     }
 
     componentWillReceiveProps(_props, ...args) {
       const [spyKey, spyDepth, props] = popSpyKey(_props);
-      spy.keyMap.set(this, spyKey);
-      spy.depthMap.set(this, spyDepth);
+      context.keyMap.set(this, spyKey);
+      context.depthMap.set(this, spyDepth);
       if (super.componentWillReceiveProps) {
         super.componentWillReceiveProps(props, ...args);
       }
     }
 
     render(...args) {
-      const spyKey = spy.keyMap.get(this);
-      const spyDepth = spy.depthMap.get(this);
-      return spyWalk(spy, setVDom(spy, spyKey, super.render(...args)), spyDepth + 1);
+      const spyKey = context.keyMap.get(this);
+      const spyDepth = context.depthMap.get(this);
+      return spyWalk(context, setVDom(context, spyKey, super.render(...args)), spyDepth + 1);
     }
   }
   return Spy;
 };
 
-const createNoopSpy = (spy, Component) => {
-  if (spy.componentNoopMap.get(Component)) {return spy.componentNoopMap.get(Component);}
+const createNoopSpy = (context, Component) => {
+  if (context.componentNoopMap.get(Component)) {return context.componentNoopMap.get(Component);}
 
-  const Spy = _createNoopSpy(spy, Component);
+  const Spy = _createNoopSpy(context, Component);
   Spy.isSpy = true;
 
-  spy.componentNoopMap.set(Component, Spy);
+  context.componentNoopMap.set(Component, Spy);
 
   return Spy;
 };
 
-const createSpy = (spy, Component) => {
-  if (spy.componentMap.get(Component)) {return spy.componentMap.get(Component);}
+const createSpy = (context, Component) => {
+  if (context.componentMap.get(Component)) {return context.componentMap.get(Component);}
 
   let Spy;
   if (!Component.prototype.render) {
-    Spy = createFuncSpy(spy, Component);
+    Spy = createFuncSpy(context, Component);
   }
   else {
-    Spy = createClassSpy(spy, Component);
+    Spy = createClassSpy(context, Component);
   }
 
   Spy.isSpy = true;
 
-  spy.componentMap.set(Component, Spy);
+  context.componentMap.set(Component, Spy);
 
   return Spy;
 };
 
-class SpyWrapper {
+class RenderContext {
   constructor({depth}) {
     this.renderedDepth = (depth || Infinity) - 1;
 
@@ -173,14 +173,14 @@ const vdomFilter = (pred, vdomMap, vdom) => {
 };
 
 class FindWrapper {
-  constructor(spy, _iter, selector) {
-    this.spy = spy;
+  constructor(context, _iter, selector) {
+    this.context = context;
     this.length = 0;
     let iter = _iter;
     if (selector) {
       this.selector = selector;
       iter = [].concat(...Array.from(iter, root => (
-        Array.from(vdomFilter(isWhere(selToWhere(selector)), spy.vdomMap, root))
+        Array.from(vdomFilter(isWhere(selToWhere(selector)), context.vdomMap, root))
       )));
     }
     iter
@@ -191,7 +191,7 @@ class FindWrapper {
   }
 
   at(index) {
-    return new FindWrapper(this.spy, [this[index]]);
+    return new FindWrapper(this.context, [this[index]]);
   }
 
   attr(name) {
@@ -221,7 +221,7 @@ class FindWrapper {
    * Return the text of all nested children concatenated together.
    */
   text() {
-    return Array.from(vdomWalk(this.spy.vdomMap, Array.from(this)))
+    return Array.from(vdomWalk(this.context.vdomMap, Array.from(this)))
     // Filter for strings (text nodes)
       .filter(value => typeof value === 'string')
     // Concatenate all strings together
@@ -229,7 +229,7 @@ class FindWrapper {
   }
 
   contains(vdom) {
-    return Array.from(vdomWalk(this.spy.vdomMap, Array.from(this)))
+    return Array.from(vdomWalk(this.context.vdomMap, Array.from(this)))
       .filter(value => isEqual(vdom, value))
       .length > 0;
   }
@@ -251,7 +251,7 @@ class FindWrapper {
   }
 }
 
-const deep = (vdom, {depth = Infinity} = {}) => new SpyWrapper({depth}).render(vdom);
+const deep = (vdom, {depth = Infinity} = {}) => new RenderContext({depth}).render(vdom);
 const shallow = vdom => deep(vdom, {depth: 1});
 
 exports = module.exports = deep;
