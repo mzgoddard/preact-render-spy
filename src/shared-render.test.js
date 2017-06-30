2e76f1e1303b2cb7578be863450f2707a7d1d557
Michael "Z" Goddard <mzgoddard@gmail.com>
2017-06-30 03:19:29 -0400

    Test the FindWrapper methods and their convenience SpyWrapper ones

--- src/shared-render.test.js
+++ src/shared-render.test.js
@@ -87,6 +87,73 @@ const sharedTests = (name, func) => {
     const context = func(<div><NullStateless />text</div>);
     expect(context.find('div').text()).toBe('text');
   });
+
+  it(`${name}: at() returns the indexed member`, () => {
+    const context = func(<div><div /><div /></div>);
+    expect(() => context.at(0)).not.toThrow();
+    expect(() => context.at(1)).toThrow();
+    expect(context.at(0)[0]).toEqual(<div><div /><div /></div>);
+    expect(() => context.find('div').at(2)).not.toThrow();
+    expect(() => context.find('div').at(3)).toThrow();
+    expect(context.find('div').at(1)[0]).toEqual(<div />);
+  });
+
+  it(`${name}: attr() returns the attribute value`, () => {
+    const context = func(<DivChildren count={1}><div class="first" /><div class="second" /></DivChildren>);
+    expect(() => context.find('div').attr('class')).toThrow();
+    expect(() => context.find('span').attr('class')).toThrow();
+    expect(context.attr('count')).toBe(1);
+    expect(context.find('.first').attr('class')).toBe('first');
+  });
+
+  it(`${name}: attrs() returns all attributes as an object`, () => {
+    const context = func(<DivChildren count={1} value={'abc'}><div class="first" name="first" /><div class="second" name="second" /></DivChildren>);
+    expect(() => context.find('div').attrs()).toThrow();
+    expect(() => context.find('span').attrs()).toThrow();
+    expect(context.attrs()).toEqual({count: 1, value: 'abc'});
+    expect(context.find('.first').attrs()).toEqual({class: 'first', name: 'first'});
+  });
+
+  it(`${name}: contains() return true if a virtual dom is in the tree`, () => {
+    const context = func(<DivChildren><div class="first" /><span class="second"><div class="third" /></span></DivChildren>);
+    expect(context.contains(<div class="first" />)).toBeTruthy();
+    expect(context.contains(<span class="second" />)).toBeFalsy();
+    expect(context.contains(<DivChildren />)).toBeFalsy();
+    expect(context.contains(<DivChildren><div class="first" /><span class="second"><div class="third" /></span></DivChildren>)).toBeTruthy();
+  });
+
+  it(`${name}: filters components`, () => {
+    const context = func(<div><NullStateless class="first" /><NullStateless class="second" /></div>);
+    expect(context.find('NullStateless').length).toBe(2);
+    expect(context.find('NullStateless').filter('.first').length).toBe(1);
+    expect(context.filter('div').length).toBe(1);
+    expect(context.filter('span').length).toBe(0);
+  });
+
+  it(`${name}: output returns vdom output by a Component`, () => {
+    const context = func(<DivChildren><span /></DivChildren>);
+    expect(() => context.find('div').output()).toThrow();
+    expect(context.output()).toEqual(<div><span /></div>);
+    expect(context.find('DivChildren').output()).toEqual(<div><span /></div>);
+  });
+
+  it(`${name}: simulate an event`, () => {
+    let count = 0;
+    let context = func(<div onClick={() => {count++;}}/>);
+    expect(count).toBe(0);
+    context.simulate('click');
+    expect(count).toBe(1);
+    context = func(<ClickCount />);
+    expect(context.contains('0')).toBeTruthy();
+    context.find('div').simulate('click');
+    expect(context.contains('1')).toBeTruthy();
+  });
+
+  it(`${name}: all the text of the virtual dom`, () => {
+    const context = func(<DivChildren><div>foo</div><div>bar</div></DivChildren>);
+    expect(context.text()).toBe('foobar');
+    expect(context.find('div').at(0).text()).toBe('foobar');
+  });
 };
 
 sharedTests('deep', deep);
