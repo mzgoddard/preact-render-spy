3cd2cd3d55612b8e4220e0c0e05dc44806f4b1f8
Michael "Z" Goddard <mzgoddard@gmail.com>
2017-07-07 11:09:01 -0400

    Stop using babel-preset-env

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = ({
         exclude: [join(__dirname, 'node_modules')],
         loader: 'babel-loader',
         options: {
-          presets: ['preact', 'env'],
+          presets: ['preact'],
         },
       },
     ],
