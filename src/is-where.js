3189ffa8ab4b48bf6a7c588d34da786e9109c9e8
Corey Frang <gnarf37@gmail.com>
2017-10-17 11:59:34 -0400

    Remove array checks, add docblock

--- src/is-where.js
+++ src/is-where.js
@@ -2,6 +2,29 @@ const entries = require('object.entries');
 
 const ATTRIBUTE_PRESENT = {exists: true};
 
+/**
+ * _isWhere matching checks
+ * `where` should be an object with keys you wish to match against a `target` object
+ *
+ * { attr: ATTRIBUTE_PRESENT } - If the value of a key is the ATTRIBUTE_PRESENT constant
+ *                               we return false if the attribute is not a property of that
+ *                               object. `'attr' in target`
+ *
+ * { attr: null } - Asserts that `target.attr === null`
+ *
+ * { attr: anotherObject } - Recurses using `_isWhere(where.attr, target.attr)`
+ *
+ * { nodeName: String|Function } - Matches value checking `target.nodeName`.
+ *     When `target.nodeName` is a function, it tests that `name`, `displayName` match the
+ *     String given, or when a Function, that they are equal.
+ *
+ * { class: String } or { className: String } - Tests the `target.class || target.className` for
+ *     the presence of the given string.  Will "split" the target class attribute into an array on
+ *     whitespace, and returns false if the given class isn't present.
+ *
+ * { attr: value } - All other value/attribute combinations are a simple === test
+ */
+
 const _isWhere = (where, target) => {
   // Check each key from where
   for (const [key, value] of entries(where)) {
@@ -14,15 +37,6 @@ const _isWhere = (where, target) => {
     // Allow a way to check for the pressenece of an attribute with that name.
     if (value === ATTRIBUTE_PRESENT) {
       if (!(key in target)) {
-        return false;
-      }
-      continue;
-    }
-
-    // Array checks
-    if (Array.isArray(value)) {
-      // [x,y,z] - allow the target value to be x, y, OR z
-      if (value.length && !value.some(test => target[key] === test)) {
         return false;
       }
       continue;
